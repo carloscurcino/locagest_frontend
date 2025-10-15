a0247c2da06ddeb23c989ba63e046f9dd63023a7
carloscurcino <carlosdanielf541@gmail.com>
2025-10-15 16:15:52 -0300

    Feat: adding login route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,9 +4,11 @@ import { VehicleRegistrationComponent } from './vehicle-registration.component';
 import { ClientRegistrationComponent } from './client-registration.component';
 import { StartRentalComponent } from './start-rental.component';
 import { EndRentalComponent } from './end-rental.component';
+import { LoginComponent } from './auth/login/login.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'rentals', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
   { path: 'rentals', component: RentalManagementComponent },
   { path: 'vehicles', component: VehicleRegistrationComponent },
   { path: 'clients', component: ClientRegistrationComponent },
